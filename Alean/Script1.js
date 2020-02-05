eae89e5edd57e971944df8613afcd0e289c71726
RonaldAlean <36340895+RonaldAlean@users.noreply.github.com>
2020-02-04 18:34:27 -0600

    Add files via upload

---
+++ Alean/Script1.js
@@ -0,0 +1,32 @@
+
+
+function MyFunction(){
+
+    var firstName = document.getElementById("firstName").value;
+   // alert(firtName);
+
+    document.getElementById("result").innerHTML = "My name: " + firstName;
+}
+
+/*function ThisisMyFunction() {
+
+    alert("HEllo this works with JS");
+    document.writeln("woo woo");
+
+    var x;
+    var y = 0;
+    y = 4; // change datatype
+}
+//scope who can use and see
+
+var a = 1;
+
+function one() {
+    alert("Hello local scope");
+}
+
+/*function four{
+    if (true) {
+       var a = 4;
+    }
+}*/
